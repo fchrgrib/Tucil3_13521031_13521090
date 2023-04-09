a98ea5f29da0990b04f3a6745e737aeeb604d34d
fchrgrib <16521492@mahasiswa.itb.ac.id>
2023-04-09 15:58:28 +0700

    add priorityqueue for all algorithm

--- src/algorithm/Astar.py
+++ src/algorithm/Astar.py
@@ -1,43 +1,39 @@
-def has_destination(arr, destination):
-    return arr[destination] > 0
-
-
-def minimum_edge(arr, arr_all, distance_start_point, idx_end_point):
-    index = []
-    arr_cpy = []
-
-    for i in range(len(arr)):
-        if arr[i] > 0:
-            arr_cpy.append(arr[i] + abs(arr_all[i][idx_end_point]) + distance_start_point)
-            index.append(i)
-
-    min = arr_cpy[0]
-    idx = 0
-
-    for i in range(len(arr_cpy)):
-        if min > arr_cpy[i]:
-            min = arr_cpy[i]
-            idx = i
-    return arr[index[idx]], index[idx]
-
+from queue import PriorityQueue
 
 # masukkan arr sebagai matriks ketetanggaan origin sebagai index awal dan destination sebagai index tujuan
 def a_star(arr, origin, destination):
     rute = [origin]
 
-    if has_destination(arr[origin], destination):
+    if arr[origin][destination] > 0:
         rute.append(destination)
         return rute
     else:
-        i = origin
-        distance_start_point = 0
-        while not has_destination(arr[i], destination):
-            min, idx = minimum_edge(arr[i], arr, distance_start_point, destination)
 
-            rute.append(idx)
-            i = idx
+        count = 0
+        temp_arr = [origin]
+        prioqueue = PriorityQueue()
+        prioqueue.put((abs(arr[origin][destination]), count, temp_arr))
+        count += 1
+        temp = prioqueue.get()
 
-            distance_start_point += min
+        while arr[temp[2][len(temp[2]) - 1]][destination] != 0:
 
-    rute.append(destination)
+            for i in range(len(arr[temp[2][len(temp[2]) - 1]])):
+                if arr[temp[2][len(temp[2]) - 1]][i] > 0:
+                    temp_push = temp[2].copy()
+                    temp_push.append(i)
+
+                    temp_value = 0
+                    for j in range(len(temp_push) - 1):
+                        temp_value += arr[j][j + 1]
+
+                    prioqueue.put((abs(arr[i][destination]) + temp_value, count, temp_push.copy()))
+                    count += 1
+                    temp_push.clear()
+
+            temp = prioqueue.get()
+
+        rute = temp[2]
     return rute
+
+
--- src/algorithm/CSU.py
+++ src/algorithm/CSU.py
@@ -1,41 +1,34 @@
-def has_destination(arr, destination):
-    return arr[destination] > 0
-
-
-def minimum_edge(arr, arr_all, idx_end_point):
-    index = []
-    arr_cpy = []
-
-    for i in range(len(arr)):
-        if arr[i] > 0:
-            arr_cpy.append(abs(arr_all[i][idx_end_point]))
-            index.append(i)
-
-    min = arr_cpy[0]
-    idx = 0
-
-    for i in range(len(arr_cpy)):
-        if min > arr_cpy[i]:
-            min = arr_cpy[i]
-            idx = i
-
-    return index[idx]
-
+from queue import PriorityQueue
 
 # masukkan arr sebagai matriks ketetanggaan origin sebagai index awal dan destination sebagai index tujuan
 def csu(arr, origin, destination):
     rute = [origin]
 
-    if has_destination(arr[origin], destination):
+    if arr[destination] > 0:
         rute.append(destination)
         return rute
     else:
-        i = origin
 
-        while not has_destination(arr[i], destination):
-            idx = minimum_edge(arr[i], arr, destination)
-            i = idx
-            rute.append(idx)
+        count = 0
+        temp_arr = [origin]
+        prioqueue = PriorityQueue()
+        prioqueue.put((abs(arr[origin][destination]), count, temp_arr))
+        count += 1
+        temp = prioqueue.get()
 
-    rute.append(destination)
+        while arr[temp[2][len(temp[2]) - 1]][destination] != 0:
+
+            for i in range(len(arr[temp[2][len(temp[2]) - 1]])):
+                if arr[temp[2][len(temp[2]) - 1]][i] > 0:
+                    temp_push = temp[2].copy()
+                    temp_push.append(i)
+
+                    prioqueue.put((abs(arr[i][destination]), count, temp_push.copy()))
+                    count += 1
+                    temp_push.clear()
+
+            temp = prioqueue.get()
+
+        rute = temp[2]
     return rute
+
