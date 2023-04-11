1a0b883d35aef1f12a0ba1d8f4d3e4d7aa99751e
fchrgrib <16521492@mahasiswa.itb.ac.id>
2023-04-11 14:30:01 +0700

    revition FileReader.py

--- src/algorithm/FileReader.py
+++ src/algorithm/FileReader.py
@@ -3,18 +3,18 @@ import os
 
 #file reader otomatis membaca txt di folder test di file graph.txt
 def file_reader():
-    matrix = []
-    coordinate = []
-    test = ""
-
     with open(os.path.abspath("../test/graph.txt"), 'r') as f:
         test = f.read()
     split_new_line = test.split("\n")
 
-    for i in range(len(split_new_line)//2):
+    matrix = []
+    coordinate = []
+    index = int(test[0])+1
+
+    for i in range(1, index):
         coordinate.append([int(split_new_line[i][0]), int(split_new_line[i][2])])
 
-    for i in range(len(split_new_line)//2 ,len(split_new_line)):
+    for i in range(index, 2*index-1):
         matrix.append(split_new_line[i].split(" "))
 
     for i in range(len(matrix)):
